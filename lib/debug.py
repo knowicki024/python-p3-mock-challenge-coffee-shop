35e313882a491344c26924a45f9b81ce2f9b6107
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-18 11:09:49 -0500

    mock CC

--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,20 @@ from classes.many_to_many import Coffee
 if __name__ == '__main__':
     print("HELLO! :) let's debug")
 
-    ipdb.set_trace()
+cust1 = Customer("Katie")
+cust2 = Customer("Killian")
+cust3 = Customer("Emily")
+
+c_1 = Coffee("Cold Brew")
+c_2 = Coffee("Cafe au Lait")
+c_3 = Coffee("Americano")
+
+o1 = Order(cust1, c_2, 7.6)
+o2 = Order(cust3, c_1, 8.2)
+o3 = Order(cust2, c_3, 9.2)
+
+
+
+
+
+ipdb.set_trace()
